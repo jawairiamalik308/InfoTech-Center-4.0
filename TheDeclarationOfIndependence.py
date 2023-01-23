d5b94fdb0c5009e05e779602de405588f53c7efb
Jawairia Malik <jawairiamalik268@gmail.com>
2023-01-23 13:49:39 -0500

    Update welcome message

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -2,3 +2,13 @@
 # Date : 1.20.2023
 # Program : InfoTechCenter Upgrades
 
+
+"""
+Our welcome screen will start our program letting  drivers know that the
+info tech center 4.0 OS is loading
+"""
+
+print("\n\nWelcome - InfoTech Center 4.0\n")
+
+print("InfoTech Center 4.0 OS is loading...")
+
