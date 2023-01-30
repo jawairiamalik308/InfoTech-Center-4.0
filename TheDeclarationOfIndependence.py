2eb792b53c8c2073d587e7a40ca4d454a18810ce
Jawairia Malik <jawairiamalik268@gmail.com>
2023-01-30 14:18:44 -0500

    gasoline stuff functions

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,4 +1,40 @@
 # Programmer : JoJo Malik
-# Date : 1.20.2023
-# Program : InfoTechCenter Upgrades
+# Date : 1.30.2023
+# Program : InfoTechCenter Gasoline
+
+"""
+We will make a Function that will tell us our fuel gauge level
+    -Make a list with Gas Levels
+    -Randomize the list to determine our gas level
+
+Create a Function to determine our closest gas station
+    -list of gas stations
+    -randomize and choose from list, a gas stations
+
+Create a Function to determine our gas level and closest gas station
+    -print gas level
+    -print closest gas station
+"""
+
+
+# Import Libraries here
+
+
+import random
+
+
+# Gas level Function
+
+def gas_level_gauge():
+    gas_level_list = ["Empty","Low","Quarter","Half","Three Quarter","Full"]
+    current_gas_level = random.choice (gas_level_list)
+    return current_gas_level
+
+print(gas_level_gauge())
+
+
+
+
+
+
 
