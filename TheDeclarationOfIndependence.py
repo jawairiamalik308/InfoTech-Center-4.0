f6eff1725f8e69587d09557ca159f16eeb248610
Jawairia Malik <jawairiamalik268@gmail.com>
2023-02-09 13:51:42 -0500

    Defined weather function and alerts

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,7 +1,24 @@
-# Programmer : JoJo Malik
-# Date : 1.20.2023
-# Program : InfoTechCenter Upgrades
-
 # Programmer: Jawairia Malik
 #Date: 2.8.2023
-#Program: Weather
+#Program: Weather System Updates
+
+#Import Libraries Here
+import random
+
+# Create weather conditions in a list and choose it randomly
+
+def weather():
+  weatherForecast =   ["Snowing", "Blizzard", "Raining", "Sunny", "Partly Cloudy", "Sleet", "Tornado"]
+  weatherCondition = random.choice(weatherForecast) 
+  return weatherCondition 
+
+
+#Variable to call weather() function once in our VRS() system(VRS is vehicle response system)
+weatherAlert = weather() 
+
+print(weatherAlert)
+
+#VRS() to respond to the weather conditions
+
+def v_r_s(): 
+    print("Howdy")
