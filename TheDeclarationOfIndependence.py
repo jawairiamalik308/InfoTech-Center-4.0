291cadab78b660e407b0d82f4f7edc25e667ca39
Jawairia Malik <jawairiamalik268@gmail.com>
2023-02-13 13:55:44 -0500

    more weather options added
    we added  elif and one else statements.

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -8,7 +8,7 @@ import random
 # Create weather conditions in a list and choose it randomly
 
 def weather():
-  weatherForecast =   ["Heavy Snow", "Blizzard", "Rain", "Sunny", "Partly Cloudy", "Sleet", "Tornado"]
+  weatherForecast =   ["Heavy Snow", "Blizzard", "Raining", "Sunny", "Partly Cloudy", "Sleet", "Tornado"]
   weatherCondition = random.choice(weatherForecast) 
   return weatherCondition 
 
@@ -16,7 +16,7 @@ def weather():
 #Variable to call weather() function once in our VRS() system(VRS is vehicle response system)
 weatherAlert = weather() 
 
-weatherAlert = "Heavy Snow"
+
 
 
 #VRS() to respond to the weather conditions
@@ -24,7 +24,31 @@ weatherAlert = "Heavy Snow"
 def v_r_s(): 
   if weatherAlert == "Heavy Snow":
     print("\nNWS has changed your alarm by 15 minutes due to", weatherAlert)
-    print("\nYour VRS has been engaged only allowing your vehicle to go 45 MPH")
+    print("Your VRS has been engaged only allowing your vehicle to go 45 MPH")
+  elif weatherAlert == "Blizzard":
+    print("\nNWS has changed your alarm by 45 minutes due to", weatherAlert)
+    print("VRS has been engaged only allowing your car to go 20 MPH")
+  elif weatherAlert == "Raining":
+    print("\nIt is", weatherAlert)
+    print("Bring an umbrella today")
+  elif weatherAlert == "Sunny":
+    print("\nIt is", weatherAlert)
+    print("Air quality is good today.")
+  elif weatherAlert == "Partly Cloudy":
+    print("\nIt is", weatherAlert)
+  elif weatherAlert == "Sleet":
+    print("\nNWS has changed your alarm by 30 minutes due to", weatherAlert)
+    print("VRS has been engaged only allowing your car to go 20 MPH")
+  else:
+    print("\nNWS has changed your alarm by one hour due to", weatherAlert)
+    print("Take shelter")
+
+v_r_s()
 
 
-print(v_r_s())
+
+
+
+
+
+
