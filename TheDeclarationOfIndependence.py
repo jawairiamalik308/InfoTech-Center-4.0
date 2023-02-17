d0166cdcf4b41fa817db7dcb762b22286fe926d7
Jawairia Malik <jawairiamalik268@gmail.com>
2023-02-17 13:41:56 -0500

    Added print statement

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,11 +1,4 @@
 
-
-
-
-
-
-
-
 # Programmer : JoJo Malik
 # Date : 1.30.2023
 # Program : InfoTechCenter Gasoline
@@ -23,28 +16,27 @@ Create a Function to determine our gas level and closest gas station
     -print gas level
     -print closest gas station
 """
+import time
 
-
-# Import Libraries here
-
-# Gas level Function
+#Gas level Function
 
 def gas_level_gauge():
+
     gas_level_list = ["Empty","Low","Quarter tank","Half tank","Three Quarter tank","Full"]
     current_gas_level = random.choice (gas_level_list)
     return current_gas_level
 
-#Variable calling gas level gauge function to store its value
-gas_level_indicator = gas_level_gauge()
+    gas_level_indicator = gas_level_gauge()
 
 
 #List of gas stations function
 
 
-def list_of_gas_stations(): 
+def list_of_gas_stations():
     gas_stations = ["Shell","Speedway","Costco","Sams Club","Circle K","Meijer","Marathon"]
     gas_station_nearby = random.choice (gas_stations)
     return gas_station_nearby
+
 
 
 
@@ -55,19 +47,19 @@ def gas_level_alert():
     miles_to_gas_station_quarter = round(random.uniform(26, 50), 2)
     if gas_level_indicator == "Empty":
         print("\n***WARNING, GAS IS EMPTY***")
-        sleep(1)
+        time.sleep(1)
         print("Calling Emergency Contact...")
     elif gas_level_indicator == "Low":
         print("\n***WARNING, GAS IS LOW***")
-        sleep(1)
+        time.sleep(1)
         print("\nSearching for nearest gas station...")
-        sleep(2)
+        time.sleep(2)
         print("\nNearest gas station is",list_of_gas_stations(),",",miles_to_gas_station_low,"miles away")
     elif gas_level_indicator == "Quarter tank":
         print("\nYou have quarter tank")
-        sleep(1)
+        time.sleep(1)
         print("\nSearching for nearest gas station...")
-        sleep(2)
+        time.sleep(2)
         print("\nNearest gas station is",list_of_gas_stations(),",",miles_to_gas_station_quarter,"miles away" )
     elif gas_level_indicator == "Half tank":
         print("\nHalf tank, good level of gas.")
@@ -76,6 +68,27 @@ def gas_level_alert():
     else:
         print("\nYour gas is full.")
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import random
+
+# Import Libraries here
 
 
 # Programmer: Jawairia Malik
@@ -89,14 +102,13 @@ def gas_level_alert():
 def weather():
 
 
-
   weatherForecast =   ["Heavy Snow", "Blizzard", "Raining", "Sunny", "Partly Cloudy", "Sleet", "Tornado"]
   weatherCondition = random.choice(weatherForecast) 
   return weatherCondition 
 
 
 #Variable to call weather() function once in our VRS() system(VRS is vehicle response system)
-weatherAlert = weather() 
+weatherAlert = weather()
 
 
 
@@ -136,16 +148,21 @@ def weatherTemp():
     tempCold = round(random.uniform(10, 40))
     if weather == "Heavy Snow":
         print("\nIt is",tempCold,"degrees outside")
+        print("Your heat has turned ON")
     elif weather == "Blizzard":
         print("\nIt is",tempCold,"degrees outside")
+        print("Your heat has turned ON")
     elif weather == "Raining":
         print("\nIt is",tempCold,"degrees outside")
     elif weather == "Sunny":
         print("\nIt is",tempHot,"degrees outside")
+        print("Your AC is turned ON")
     elif weather == "Partly Cloudy":
         print("\nIt is",tempHot,"degrees outside")
+        print("Your AC has been turned ON")
     elif weather == "Sleet":
         print("\nIt is",tempCold,"degrees outside")
+        print("Your heat has been turned ON")
     else:
         print("\nIt is",tempHot,"degrees outside")
 
@@ -158,9 +175,7 @@ weatherTemp()
 
 
 
-
-
 # Call Functions Here
 v_r_s()
-gas_level_alert()
 
+weatherTemp()
