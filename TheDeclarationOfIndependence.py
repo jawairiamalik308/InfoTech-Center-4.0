ed3058cf346c25d4b7eeebd0f605b1fee7f01e89
Jawairia Malik <jawairiamalik268@gmail.com>
2023-02-20 13:57:50 -0500

    Deleted weatherTemp()

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -3,82 +3,6 @@
 # Date : 1.30.2023
 # Program : InfoTechCenter Gasoline
 
-"""
-We will make a Function that will tell us our fuel gauge level
-    -Make a list with Gas Levels
-    -Randomize the list to determine our gas level
-
-Create a Function to determine our closest gas station
-    -list of gas stations
-    -randomize and choose from list, a gas stations
-
-Create a Function to determine our gas level and closest gas station
-    -print gas level
-    -print closest gas station
-"""
-import time
-
-#Gas level Function
-
-def gas_level_gauge():
-    gas_level_list = ["Empty","Low","Quarter tank","Half tank","Three Quarter tank","Full"]
-    current_gas_level = random.choice (gas_level_list)
-    return current_gas_level
-
-    gas_level_indicator = gas_level_gauge()
-
-
-#List of gas stations function
-
-
-def list_of_gas_stations():
-    gas_stations = ["Shell","Speedway","Costco","Sams Club","Circle K","Meijer","Marathon"]
-    gas_station_nearby = random.choice (gas_stations)
-    return gas_station_nearby
-
-
-
-
-# Determine gas level and closest gas station
-
-def gas_level_alert():
-    miles_to_gas_station_low = round(random.uniform(1, 25), 2)
-    miles_to_gas_station_quarter = round(random.uniform(26, 50), 2)
-    if gas_level_indicator == "Empty":
-        print("\n***WARNING, GAS IS EMPTY***")
-        time.sleep(1)
-        print("Calling Emergency Contact...")
-    elif gas_level_indicator == "Low":
-        print("\n***WARNING, GAS IS LOW***")
-        time.sleep(1)
-        print("\nSearching for nearest gas station...")
-        time.sleep(2)
-        print("\nNearest gas station is",list_of_gas_stations(),",",miles_to_gas_station_low,"miles away")
-    elif gas_level_indicator == "Quarter tank":
-        print("\nYou have quarter tank")
-        time.sleep(1)
-        print("\nSearching for nearest gas station...")
-        time.sleep(2)
-        print("\nNearest gas station is",list_of_gas_stations(),",",miles_to_gas_station_quarter,"miles away" )
-    elif gas_level_indicator == "Half tank":
-        print("\nHalf tank, good level of gas.")
-    elif gas_level_indicator == "Three Quarter tank":
-        print("\nYour gas levels are good. You have three quarters of a tank.")
-    else:
-        print("\nYour gas is full.")
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -165,7 +89,6 @@ def weatherTemp():
     else:
         print("\nIt is",tempHot,"degrees outside")
 
-weatherTemp()
 
 
 
@@ -178,4 +101,3 @@ weatherTemp()
 v_r_s()
 
 weatherTemp()
-gas_level_alert()
