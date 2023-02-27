549cfdd1c027bf9ed449c8f52674811758cba845
Jawairia Malik <jawairiamalik268@gmail.com>
2023-02-27 14:08:04 -0500

    added description to music brnach

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,4 +1,10 @@
 # Programmer : JoJo Malik
 # Date : 1.20.2023
-# Program : InfoTechCenter Upgrades
+# Program : Music
 
+
+"""
+In this branch, I am making a music play/media player.
+I will explain more later on in my code
+"""
+#Import Libraries here
